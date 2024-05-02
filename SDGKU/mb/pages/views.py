3a3a7371979a6bcd5b9212522511481a462fc075
Kory Plotts <koryplotts@yahoo.com>
2024-05-01 19:39:43 -0700

    final

--- SDGKU/mb/pages/views.py
+++ SDGKU/mb/pages/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import TemplateView
+
+    
+class PostListView(TemplateView):
+    template_name = "pages/list.html"
+
+class PostDetailView(TemplateView):
+    template_name = "pages/detail.html"
+    
+class PostCreateView(TemplateView):
+    template_name = "pages/new.html"
 
 # Create your views here.
